c46030c09b1150e12fb825cb653dc3afa8023e48
Karl Brewster <mark.moehring@outlook.com>
2022-05-01 12:26:25 -0500

    deleting branches

--- app.js
+++ app.js
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-
-// const animalArray = ['dog', 'cat', 'pig'];
-
-// animalArray.push('cow');
-
-// const personObj = {
-// name: 'Lernantino',
-// age: 99
-// };
-
-// personObj.age = 100;
-// personObj.occupation = 'Developer';
-
-const printProfileData = profileDataArr => {
-    
-    //This...
-
-    for (let i = 0; i < profileDataArr.length; i++) {
-      console.log(profileDataArr[i]);
-    }
-
-    console.log('================');
-
-    //Is the same as this...
-
-    profileDataArr.forEach((profileItem) => {
-
-        console.log(profileItem)
-
-    });
-  };
-
-printProfileData(profileDataArgs);
-=======
 const fs = require('fs');
 const generatePage = require('./src/page-template');
 
@@ -53,4 +16,3 @@ fs.writeFile('./index.html', pageHTML, err => {
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
->>>>>>> b1d8e3a (9-2 snapshot)
